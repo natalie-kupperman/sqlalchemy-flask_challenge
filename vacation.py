8520c16e256ef3f541a40677e42d8040e23769ec
Natalie Kramer <natalie.kramer@mac.com>
2018-04-15 19:41:45 -0500

    working flask script

--- vacation.py
+++ vacation.py
@@ -3,24 +3,24 @@
 ##############################################
 
 # import dependencies
-import datetime as dt
-import numpy as np
-import pandas as pd
+from flask import Flask, jsonify
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect, func
 
-from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-###############################################
+##############################################
 
 # connect database
 engine = create_engine("sqlite:///hawaii.sqlite")
 inspector = inspect(engine)
 
-# reflect an existing database into a new model
+# reflect on existing database into a new model
 Base = automap_base()
 
 # reflect the tables
@@ -28,122 +28,115 @@ Base.prepare(engine=engine, reflect=True)
 
 # save references to variables
 station = Base.classes.station_info
-measurements = Base.classes.climate_measurements
+measurement = Base.classes.climate_measurements
 
 # create session from python to the database
 session = Session(bind=engine)
 
-###############################################
-
 # flask set-up
 app = Flask(__name__)
 
-###############################################
+##############################################
 
 # flask routes
 
 @app.route("/")
-def welcome():
+def home():
     print("Server received request for 'Home' page...")
-    
-    """List all available api routes"""
-    
-    return ("Welcome to the Hawaii Vacation Climate Analysis API<br/><br/>"
-            
-                "Here are the available end points:<br/>"
-    		
-                "/api/v1.0/precipitation<br/>"
-    		
-                "/api/v1.0/stations<br/>"
-    		
-                "/api/v1.0/tobs<br/>"
-    		
-                "/api/v1.0/*enter start date in Y-m-d format*<br/>"
-    		
-                "/api/v1.0/*enter start date in Y-m-d format*/*enter end date in Y-m-d format*<br/>"
-            )
 
+    """list all available api routes"""
+
+    return ("Welcome to the Hawaii Surfer's Analysis site<br/><br/>"
+    		"Here are the available end points:<br/>"
+
+    		"/api/v1.0/precipitation<br/>"
+
+    		"/api/v1.0/stations<br/>"
+
+    		"/api/v1.0/tobs<br/>"
+
+    		"/api/v1.0/*enter start date in Y-m-d format*<br/>"
+
+    		"/api/v1.0/*enter start date in Y-m-d format*/*enter end date in Y-m-d format*<br/>"
+        )
 
 @app.route("/api/v1.0/precipitation")
 def precip():
-    
+	
     """Query for the dates and precip observations from the last year"""
-    
-    print("Server received request for precipitation page...")
-    
-    prcp = pd.DataFrame(session.query(measurements.date, measurements.prcp).\
-           filter(measurements.date.between('2016-08-23', '2017-08-23')).all())
-                        
+
+    print("Server received request for precip page...")
+
+    prcp = pd.DataFrame(session.query(measurement.date,measurement.prcp).\
+	filter(measurement.date >= '2016-01-01', measurement.date < '2017-01-01').all())
+
     prcp = prcp.set_index('date')
-                        
     prcp = prcp.to_dict()
-
+	
     return jsonify(prcp)
-                        
-@app.route("/api.v1.0/stations")
-def stations():
-    
+
+@app.route("/api/v1.0/stations")
+def stats():
+
     """Query station names"""
-    
+
     print("Server received request for station page...")
-    
-    station = pd.DataFrame(session.query(stations.name))
-    
-    return jsonify(station)
+      
+    returns = session.query(stations.name).all()
 
-@app.route("/api.v1.0/temperature")
-def temp():
-    
+    return jsonify(returns)
+     
+@app.route("/api/v1.0/tobs")
+def tobsy():
+
     """Query temperature oberservations from the last year"""
-    
-    print("server received request for tobs page...")
-    
-    tobs = pd.DataFrame(session.query(measurements.date, measurements.tobs).\
-           filter(measurements.data.between('2016-08-23', '2017-08-23')).all())
-    
-    return jsonify(tobs)
 
-@app.route("/api.v1.0/<start>")
-def temp_input(start):
-    
-    def start_temp(start_date):
-        
-        print("Server received request for start page...")
-        data = pd.DataFrame(session.query(measurements.tobs).\
-        filter(measurements.date >= start_date).all())
+    print("Server received request for tobs page...")
+    returns = session.query(measurement.date,measurement.tobs).filter(
+              measurement.date >= '2016-01-01', measurement.date < '2017-01-01').all()
+     
+    return jsonify(returns)
 
-        avg = round(data['tobs'].mean())
-        low = data['tobs'].min()
-        high = data['tobs'].max()
-        return avg, low, high
-
-    average, minimum, maximum = start_temp(start)
-    dictionary = {"Minimum_Temp": str(minimum), "Average_Temp": str(average), "Max_Temp": str(maximum)}
-    return jsonify(dictionary)
 
 # Return a json list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 # When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
 
+@app.route("/api/v1.0/<start>")
+def temp_input(start):
+
+	def starttemp(start_date):
+
+	    print("Server received request for start page...")
+	    data = pd.DataFrame(session.query(measurement.tobs).\
+	    filter(measurement.date >= start_date).all())
+
+	    avg = round(data['tobs'].mean())
+	    low = data['tobs'].min()
+	    high = data['tobs'].max()
+	    return avg, low, high
+
+	average, minimum, maximum = starttemp(start)
+	dictionary = {"Minimum_Temp":str(minimum),"Average_Temp": str(average),"Max_Temp":str(maximum)}
+	return jsonify(dictionary)
+
 @app.route("/api/v1.0/<start>/<end>")
-# create first function to gather inputs from user in address line then pushes them into nested dunction
 def temp_input_end(start, end):
-    
-    def calc_temps(start_date, end_date):
-        print("Server received request for start/end page...")
-        data = pd.DataFrame(session.query(measurements.tobs).\
-        filter(measurements.date >= start_date, measurements.date < end_date).all())
 
-        avg = round(data['tobs'].mean())
-        low = data['tobs'].min()
-        high = data['tobs'].max()
-        return avg, low, high
+	def calc_temps(start_date, end_date):
 
-    average, minimum, maximum = calc_temps(start, end)
-    dictionary = {"Minimum_Temp" : str(minimum), "Average_Temp" : str(average), "Max_Temp" : str(maximum)}
-    
-    return jsonify(dictionary)
+		print("Server received request for start/end page...")
+		data = pd.DataFrame(session.query(measurement.tobs).\
+		filter(measurement.date >= start_date, measurement.date < end_date).all())
 
-if __name__ == '__main__':
-    app.run()
+		avg = round(data['tobs'].mean())
+		low = data['tobs'].min()
+		high = data['tobs'].max()
+		return avg,low,high
 
+	average, minimum, maximum = calc_temps(start, end)
+	dictionary = {"Minimum_Temp":str(minimum),"Average_Temp": str(average),"Max_Temp":str(maximum)}
+	return jsonify(dictionary)
+
+if __name__ == "__main__":
+    app.run(debug=True)
